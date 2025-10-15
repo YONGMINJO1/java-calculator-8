ce39afa82b2aff9294b0a98542e4876539c94b36
YONGMINJO1 <iy0594@naver.com>
2025-10-16 00:09:08 +0900

    chore: 프로젝트 기본 구조 및 환경 설정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,7 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 프로그램 구현 시작점
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 }
---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class StringAddCalculator {
+
+    public static int add(String input) {
+        // 기능
+        return 0;
+    }
+}
